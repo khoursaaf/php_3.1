a60cfa81f17b55477cd7c7a71f30522bc3cb2ba6
khoursaaf <khoursa@gmail.com>
2018-11-20 16:25:07 +0100

    correction

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>Exercise</title>
-</head>
-<body>
-  <p>Créer une variable et l'initialiser à 0.
-    Tant que cette variable n'atteint pas 10, il faut :</p>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer une variable et l'initialiser à 0.
+    Tant que cette variable n'atteint pas 10, il faut :
+    </p>
     <ol>
       <li>l'afficher</li>
       <li>l'incrementer</li>
     </ol>
-    <?php
-    $nombre = 0;
-    while ($nombre <= 10) {
-      echo "$nombre <br />";
-      $nombre++;
-    }
-     ?>
+    <p>1er test</p>
+    <?php $number = 0;
+    while ($number <= 10)
+    { ?>
+    <p><?php echo $number;
+    $number++;
+    }?></p>
+    <p>2eme test</p>
+    <?php for ($number=0; $number <= 10; $number++) { ?>
+    <p><?php echo $number; } ?></p>
   </body>
-  </html>
+</html>
